23a79759ac283125532e6f5c1752fa4caa09a09f
naveen <naveenmk675@gmail.com>
2024-02-29 19:10:49 +0530

    fetched data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,20 @@ import React, { useEffect, useState } from 'react'
 import app from '../firebaseConfig'
 import { getDatabase,get,ref } from 'firebase/database'
 import { Link } from 'react-router-dom'
+import { FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
+
 
 const Home = () => {
 
 
-  const headers=['Employee ID','Name','Age','Departmnt','Role']
+  const headers=['Employee ID','Name','Age','Department','Role']
+  const dropdownValues=[2,4,6,8,10]
   const [result,setResult]=useState([])
+
+
+  const [noOfPages,setNoOfPages]=useState(4)
+  const [page,setPage]=useState(1)
 
   const getData=async()=>{
     const db=getDatabase(app)
@@ -34,16 +42,29 @@ const Home = () => {
                 <button className='px-2 py-2 w-[50px] bg-purple-500 rounded-lg text-white'>
                 <Link className='text-sm' to='/add'>Add</Link>
                 </button>
+                <select id="page" name="page" 
+                onChange={(e)=>{setNoOfPages(e.target.value)}}
+                class="block w-full px-4 py-2 border rounded-md bg-white shadow-sm focus:outline-none focus:border-pruple-500 focus:ring focus:ring-purple-500">
+                  <option>Select Page</option>
+               {
+                    dropdownValues.map((e,i)=>{
+                      return(
+                        <option value={e} className='text-gray-600'>{e}</option>
+                      )
+                    })
+               }
+                
+              </select>
             </div>
         </div>
         <div className='overflow-x-auto mt-4'>
         <table className='w-full text-left table-auto'>
           <thead>
-            <tr className='text-md text-gray-600/80  tracking-wider'>
+            <tr className='text-md text-gray-600  tracking-wider'>
               {
                 headers.map((e,i)=>{
                   return (
-                    <th key={i} className='px-5 py-6'>{e}</th>
+                    <th key={i} className='px-4 py-6'>{e}</th>
                   )
                 })
               }
@@ -51,16 +72,16 @@ const Home = () => {
           </thead>
           <tbody>
               {
-                result.map((e,i)=>{
+                result.slice(page*noOfPages-noOfPages,noOfPages*page).map((e,i)=>{
                   return(
                     <tr key={i} className='border-t border-gray-200'>
                       <td className=' py-3 px-4 whitespace-nowrap'>
-                      {e.id}
+                      <h2 className='text-gray-600/80 text-lg font-medium'>{e.id}</h2>
                       </td>
-                      <td className=' py-3 px-4 whitespace-nowrap'>{e.name}</td>
-                      <td className=' py-3 px-4 whitespace-nowrap'>{e.age}</td>
-                      <td className=' py-3 px-4 whitespace-nowrap'>{e.department}</td>
-                      <td className=' py-3 px-4 whitespace-nowrap'>{e.role}</td>
+                      <td className=' py-3 px-4 whitespace-nowrap'> <h2 className='text-gray-600/80 text-lg font-medium'>{e.name}</h2></td>
+                      <td className=' py-3 px-4 whitespace-nowrap'> <h2 className='text-gray-600/80 text-lg font-medium'>{e.age}</h2></td>
+                      <td className=' py-3 px-4 whitespace-nowrap'> <h2 className='text-gray-600/80 text-lg font-medium'>{e.department}</h2></td>
+                      <td className=' py-3 px-4 whitespace-nowrap'> <h2 className='text-gray-600/80 text-lg font-medium'>{e.role}</h2></td>
                       
                     </tr>
                   )
@@ -69,6 +90,23 @@ const Home = () => {
           </tbody>
         </table>
         </div>
+        {
+          result.length>0 && (
+            <div className='w-full flex flex-row items-center justify-center px-2'>
+                <span>
+                  <FaChevronLeft/>
+                </span>
+                {
+                  [...Array(Math.floor(result.length/noOfPages))].map((e,i)=>{
+                    return(
+                      <span key={i} className={`p-4 cursor-pointer ${page===i+1?'bg-purple-500 rounded-lg text-white':''}`} onClick={()=>{setPage(i+1)}}>{i+1}</span>
+                    )
+                  })
+                }
+                <span><FaChevronRight/></span>
+            </div>
+          )
+        }
     </div>
   )
 }
