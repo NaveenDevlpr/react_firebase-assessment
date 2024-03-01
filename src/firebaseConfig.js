d43794b32a17a8e0e4cee68189e931fc63327d4d
naveen <naveenmk675@gmail.com>
2024-03-01 10:42:44 +0530

    final changes done

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,15 @@ import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
 
- /*apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+ /* apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID*/
+
 
   
   apiKey: "AIzaSyDMbBrS1aA_bgdHve-PbFPrYyBO0_Ynon4",
